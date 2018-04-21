a67c9bf0ccfb02b2bdbfe357f2f217d678871451
Leif Wesche <37157762+lewesche@users.noreply.github.com>
2018-04-20 22:58:58 -0700

    Cleaned Things up. Same script/funtion work in 2D and 3D, fixed shuffling

--- Big_Sun_Come_Strong.m
+++ Big_Sun_Come_Strong.m
@@ -5,61 +5,81 @@ clear all
 close all
 clc
 
-M0=800;
-x0=0; y0=0; z0=0;
-Particles=5000;
-dt=0.0333;
-run_time=240;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%% Inputs %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+M0=800;                         %Obrital Mass
+x0=0; y0=0; z0=0;               %Mass Fixed Location
+Particles=5000;                 %Number of Particles
+dt=0.0333;                      %Time step 
+run_time=240;                   %Run Time
+D=2;                            %Dimesions (2 or 3)
 
-xi=[randi([20,20], [3, Particles/2])+rand([3, Particles/2])/4, randi([20,20], [3, Particles/2])+rand([3, Particles/2])/4];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%% Setup %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Set Center Position/Mass
+if D==2
+    z0=[];
+end
+x0=[x0; y0; z0]; M=M0;
+
+%Generate Particle initial positions and speeds
+xi=[randi([20,20], [D, Particles/2])+rand([D, Particles/2])/4, randi([20,20], [D, Particles/2])+rand([D, Particles/2])/4];
 vi=[randi([5,15], [1, Particles/2])+rand([1, Particles/2]), randi([-15,-5], [1, Particles/2])-rand([1, Particles/2])];
-D=size(xi); D=D(1);
 
-%Shuffle vectors - Not Working
-ind=randperm(length(vi));
+%Shuffle Position/Velocity Vectore
+ind=randperm(length(vi)); V=[]; x=[];
 for i=ind
-    x(1,i)=xi(1,i);
-    x(2,i)=xi(2,i);
-    x(3,i)=xi(3,i);
-    V(i)=vi(i);
+    x=[x, xi(:,i)];
+    V=[V,vi(i)];
 end
 
-x0=[x0; y0; z0];
 %Calculate Initial Velocities In Cartesian Coords.
 for n=1:Particles
     for d=[1:D]
         r(d,:)=[x(d,n)-x0(d)];
     end
     if D==2
-        r(3)=[];
+        v(:,n)=V(n)*[r(2), -r(1)]/norm(r);
     end
-    v(:,n)=V(n)*[r(2), -r(1), r(3)]/norm(r);
+    if D==3
+        v(:,n)=V(n)*[r(2), -r(1), r(3)]/norm(r);
+    end
 end
 
-M=M0;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%% Iterate %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 time=0:dt:run_time;
 fig=figure('Position', [100, 50, 800, 720], 'color', [0.1, 0.1, 0.1]);
-for t=time
+%2D Animation
+if D==2
+    for t=time
+        [x, v] = BSCS_Dynamics(x, v, dt, M, x0);
     
-    %M=M0+0.25*M0*sin(t)
-    
-    [x, v] = BSCS_Dynamics(x, v, dt, M, x0);
-    
-%     plot(x0(1), x0(2), 'wo', 'linewidth', 6*M/M0); hold on
-%     plot(x(1,:), x(2,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0.9, 0, 0]); hold on
-%     set(gca, 'Color', [0.1, 0.1, 0.1])
-%     axis([-40, 40, -40, 40]); axis off
-%     pause(dt) 
-%     hold off
+        plot(x0(1), x0(2), 'wo', 'linewidth', 6*M/M0); hold on
+        plot(x(1,:), x(2,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0.9, 0, 0]); hold on
+        set(gca, 'Color', [0.1, 0.1, 0.1])
+        axis([-40, 40, -40, 40]); axis off
+        pause(dt) 
+        hold off 
+    end
+end
+%3D Animation
+if D==3
+    for t=time
+        [x, v] = BSCS_Dynamics(x, v, dt, M, x0);
 
-    plot3(x0(1), x0(2), x0(3), 'wo', 'linewidth', 6*M/M0); hold on
-    plot3(x(1,:), x(2,:), x(3,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0.9, 0, 0]); hold on
-    set(gca, 'Color', [0.1, 0.1, 0.1])
-    axis([-40, 40, -40, 40, -40, 40]); %axis off
-    
-    pause(dt)
-    hold off
+        plot3(x0(1), x0(2), x0(3), 'wo', 'linewidth', 6*M/M0); hold on
+        plot3(x(1,:), x(2,:), x(3,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0.9, 0, 0]); hold on
+        set(gca, 'Color', [0.1, 0.1, 0.1])
+        axis([-40, 40, -40, 40, -40, 40]); %axis off
+        view(-40*cos(t/12), 30*cos(t/12))
+        pause(dt)
+        hold off
+    end
 end
