150b4aabaa0a97bd2bfe3864db40c49edcf5c2b8
Leif Wesche <37157762+lewesche@users.noreply.github.com>
2018-04-22 19:39:41 -0700

    Changing colors

--- Big_Sun_Come_Strong.m
+++ Big_Sun_Come_Strong.m
@@ -16,6 +16,11 @@ dt=0.0333;                      %Time step
 run_time=240;                   %Run Time
 D=2;                            %Dimesions (2 or 3)
 
+%Backgrond
+t0=5;                           %Fade Cycle Time
+s=50;                           %Number of Background Points
+trgb=10;                         %Color Change Speed
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%% Setup %%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -52,12 +57,11 @@ end
 
 %Background Color Map
 map=[];
-map=[];
 for i=logspace(0, 1.5, 100)
     temp=[0.5, 0, 0.7]/i;
     map=[temp; map];
 end
-C=zeros(83, 83); t0=1; s=10000;
+C=zeros(83, 83); 
 [gridx, gridy]=meshgrid([-41:41], [-41:41]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -69,23 +73,32 @@ fig=figure('Position', [100, 50, 800, 720], 'color', [0, 0, 0]);
 %2D Animation
 if D==2
     C=zeros([83, 83]); C(83,83)=1;
-    ind=randi([1, 83], 2, s);
+    ind=randi([1, 82], 2, s);
     ind(3,:)=1:length(ind);
     for t=time
- 
+        
+        map=[];
+        for i=logspace(0, 1.5, 100)
+            temp=[abs(sin(t/trgb)), 0, abs(cos(t/trgb))]/i;
+            map=[temp; map];
+        end
         if round(mod(t,t0), 1) == 0
             tc=0;
         end
         tc=tc+dt;
         for i=1:length(ind)
             C(ind(1,i), ind(2,i))=(0.4*cos(tc/(t0/2)*pi+ind(3,i))+0.6);
+            if C(ind(1,i), ind(2,i)) < 0.25
+                C(ind(1,i), ind(2,i))=0;
+                ind(1:end-1,i)=randi([1, 82], 2, 1);
+            end
         end
         
         [x, v] = BSCS_Dynamics(x, v, dt, M, x0);
         
         pcolor(gridx, gridy, C); colormap(map); hold on
-        plot(x0(1), x0(2), 'wo', 'linewidth', 8*M/M0); hold on
-        plot(x(1,:), x(2,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [0.5, 0, 0.7]); hold on
+        plot(x0(1), x0(2), 'o', 'linewidth', 8*M/M0, 'Color', [abs(sin(t/trgb)), 0, abs(cos(t/trgb))]); hold on
+        plot(x(1,:), x(2,:), 'o', 'linewidth', 1, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', [abs(sin(t/trgb)), 0, abs(cos(t/trgb))]); hold on
         set(gca, 'Color', [0.1, 0.1, 0.1])
         axis([-40, 40, -40, 40]); axis off
         pause(dt) 
